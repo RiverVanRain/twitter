131ddb3b11f16bc3dc5d1829782bb8576e81025e
RiverVanRain <river.vanrain@gmail.com>
2015-07-31 02:25:35 +0430

    Update css.php

--- views/default/widgets/twitterz/css.php
+++ views/default/widgets/twitterz/css.php
@@ -5,7 +5,7 @@
 	 * @package Elgg Twitter Widgets Pack
 	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
 	 * @author RiverVanRain
-	 * @URL http://weborganizm.org/crewz/p/1983/personal-net
+	 * @URL http://o.wzm.me/crewz/p/1983/personal-net
 	 * @copyright (c) RiverVanRain 2014
 	 */
 ?>
